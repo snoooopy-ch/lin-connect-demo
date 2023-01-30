5202754a13c1a619f21af386d2663d310796a194
snoooopy-ch <snoopy.850807@gmail.com>
2023-01-30 23:10:26 +0900

    SwiperSlideのnavigationを修正

--- src/components/layouts/items/index.tsx
+++ src/components/layouts/items/index.tsx
@@ -4,7 +4,7 @@ import Item from '../../molecules/item';
 import { Align } from "../../../models"
 
 // import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import "./style.scss";
 import 'swiper/css';
@@ -12,10 +12,12 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import Good, { createDummyGood } from "../../../models/good";
+import React from "react";
+
 
 const Items: React.FC = () => {
 	const recommends = Array.from(Array(5).keys()).map(() => createDummyGood());
-
+	
 	return (
 		<section className="items-section">
 			<div className="items-section-container">
@@ -24,7 +26,6 @@ const Items: React.FC = () => {
 						title='Items'
 					/>
 				</div>
-				
 				<Swiper
 					modules={[Navigation, Pagination, Scrollbar, A11y]}
 					breakpoints={{
@@ -37,11 +38,15 @@ const Items: React.FC = () => {
 							spaceBetween: 29.5,
 						}
 					}}
-					navigation
 					pagination={{ 
 						clickable: true
 					}}
-				>
+					watchOverflow={true}
+					navigation={{
+						nextEl: '.icon-arrow-long-right',
+      			prevEl: '.icon-arrow-long-left',
+					}}
+				>	
 					{recommends.map((recommend: Good, i: number) => {
 						return (
 							<SwiperSlide
@@ -56,6 +61,8 @@ const Items: React.FC = () => {
 						)
 					})}
 				</Swiper>
+				<span className="icon-arrow-long-right swiper-button-next custom-navigation"></span>
+				<span className="icon-arrow-long-left swiper-button-prev custom-navigation"></span>
 				<div className="items-section-linkbtn">
 					<LinkButton
 						title='View more→'
